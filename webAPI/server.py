95bc8bc1f0ce31406f05214595c7c0c32fcad3af
hjoaquim <h.joaquim@campus.fct.unl.pt>
2020-11-29 23:08:55 +0000

    building api

---
+++ webAPI/server.py
@@ -0,0 +1,29 @@
+# on terminal:
+# C:\path\to\app>set FLASK_APP=hello.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run() 
+ 
+
+""" from flask import Flask, request, jsonify
+from tkinter import Image
+
+app = Flask(__name__)
+
+@app.route("/im_size", methods=["POST"])
+def process_image():
+    file = request.files['image']
+    # Read the image via file.stream
+    img = Image.open(file.stream)
+    return jsonify({'msg': 'success', 'size': [img.width, img.height]})
+
+
+if __name__ == "__main__":
+    app.run(debug=True) """
