793aa61d8063d204fc580acae2a8c938720d0c47
Amaral <amaral.pap@gmail.com>
2020-12-09 23:30:04 +0000

    Final One, its working
    Also Included the instructions in folder "FIles for Coppelia"

--- src/Transport/TransportAgent.java
+++ src/Transport/TransportAgent.java
@@ -119,7 +119,8 @@ public class TransportAgent extends Agent {
             System.out.println(myAgent.getLocalName() + ": Preparing results of transportation REQUEST");
             
             
-            myLib.executeMove(initial_position, dest_position, request.getSender().getName());
+            //myLib.executeMove(initial_position, dest_position, request.getSender().getLocalName());
+            myLib.executeMove(initial_position, dest_position, request.getSender().getLocalName());
             
             ACLMessage msg = request.createReply();
             msg.setPerformative(ACLMessage.INFORM);
